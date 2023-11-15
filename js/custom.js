22068ea6cfe88f7863261b5bff67e3e889a308b0
DESKTOP-UINH1OI\USER <max8030396@gmail.com>
2023-11-16 00:51:29 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,5 @@
 (function ($) {
   "use strict";
-  
-
-  $(document).ready(function() {
-    $('select').niceSelect();
-
-    
-  });
   // menu fixed js code
   // $(window).scroll(function () {
   //   var window_top = $(window).scrollTop() + 1;
@@ -24,75 +17,107 @@ $(document).ready(function() {
     el: '#app',
     data() {
       return {
-        message: 'Hello Vue!',
+        isOpenImageModal: false,
+        useSingleImg: false,
+        singleImgUrl: '',
         rooms: [
           {
-            room: '單人房',
-            context: '一些文案',
-            alt: '單人房照片',
-            currentImg: 'img/room/room_01.jpg',
-            roomImgs: [
-              'img/room/room_01.jpg',
-              'img/room/room_02.jpg',
-              'img/room/room_03.jpg',
-              'img/room/room_04.jpg',
-            ]
-          },
-          {
             room: '雙人房',
-            context: '一些文案',
-            alt: '單人房照片',
-            currentImg: 'img/room/room_01.jpg',
+            context: '',
             roomImgs: [
-              'img/room/room_01.jpg',
-              'img/room/room_02.jpg',
-              'img/room/room_03.jpg',
-              'img/room/room_04.jpg',
+              'img/room/房內衛浴.JPG',
             ]
           },
           {
-            room: '多人房',
-            context: '一些文案',
-            alt: '單人房照片',
-            currentImg: 'img/room/room_01.jpg',
+            room: '三人房',
+            context: '',
             roomImgs: [
-              'img/room/room_01.jpg',
-              'img/room/room_02.jpg',
-              'img/room/room_03.jpg',
-              'img/room/room_04.jpg',
+              'img/room/單床.JPG',
+              'img/room/雙床_1.JPG',
+              'img/room/房內衛浴.JPG',
             ]
           },
           {
-            room: '更多人房',
-            context: '一些文案',
-            alt: '單人房照片',
-            currentImg: 'img/room/room_01.jpg',
+            room: '四人房',
+            context: '',
+            roomImgs: ['img/room/四人房.JPG']
+          },
+          {
+            room: '家庭連通房',
+            context: '',
             roomImgs: [
-              'img/room/room_01.jpg',
-              'img/room/room_02.jpg',
-              'img/room/room_03.jpg',
-              'img/room/room_04.jpg',
+              'img/room/連通房_1.JPG',
+              'img/room/連通房_2.JPG',
+              'img/room/雙床_1.JPG',
+              'img/room/房內衛浴.JPG',
             ]
           }
-        ]
+        ],
+        events: [
+          {
+            eventId: 'event02',
+            eventName: '中秋聯歡暨家屬感情聯繫活動',
+            eventDate: '2023/09/16',
+            eventOrganizer: '社團法人中華雲鵬展弘協會 | 雲鵬長照中心',
+            imgs: [
+              'img/event/20231115_01.jpg',
+              'img/event/20231115_01.jpg',
+            ]
+          },
+          { 
+            eventId: 'event01',
+            eventName: '雲鵬長照中心',
+            eventDate: '2023/01/21',
+            eventOrganizer: '雲鵬長照中心',
+            imgs: [
+              'img/event/20231114_01.jpg',
+              'img/event/20231114_02.jpg',
+              'img/event/20231114_03.jpg',
+              'img/event/20231114_04.jpg',
+              'img/event/20231114_05.jpg',
+              'img/event/20231114_06.jpg'
+            ]
+          },
+        ],
+        modalImages: [],
+      }
+    },
+    computed: {
+      isMobile() {
+        return window.innerWidth < 991;
       }
     },
     methods: {
-      test() {
-        console.log(this.message)
+      closeImageModal() {
+        $('#imageCarouse').carousel(0)
+        $('#imageModel').modal('hide');
+      },
+      prevCarousel() {
+        $('#imageCarouse').carousel('prev');
+      },
+      nextCarousel() {
+        $('#imageCarouse').carousel('next');
+      },
+      toggleMenu(type) {
+        this.$refs[type].scrollIntoView({ behavior: 'smooth' });
+        if (this.isMobile) $('.collapse').collapse('toggle');
       },
       getYear() {
         return new Date().getFullYear();
       },
-      changeImg(room, img) {
-        room.currentImg = img;
-      },
       scrollTo(id) {
-        this.$refs[id].scrollIntoView({ behavior: 'smooth',  block: "center", });
+        this.$refs[id].scrollIntoView({ behavior: 'smooth', block: "center", });
+      },
+      openRoomImage(i, singleImgUrl) {
+        this.useSingleImg = !!singleImgUrl;
+        if(this.useSingleImg) {
+          this.singleImgUrl = singleImgUrl;
+        } else {
+          this.modalImages = this.rooms[i].roomImgs;
+        }
+        this.isOpenImageModal = true;
+        $('#imageModel').modal('show')
       }
-    },
-    created() {
-      console.log(this)
     }
   })
 });
@@ -116,11 +141,4 @@ function mailChimp() {
   $('#mc_embed_signup').find('form').ajaxChimp();
 }
 mailChimp();
-
-
-
-
 }(jQuery));
-
-
-
